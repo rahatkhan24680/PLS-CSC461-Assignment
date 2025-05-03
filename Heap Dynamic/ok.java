68063dfd663f248581403f02466c307a584ef54b
rahatkhan24680 <rahatkhan24680@gmail.com>
2025-05-03 14:55:35 -0400

    Update ok.java

--- Heap Dynamic/ok.java
+++ Heap Dynamic/ok.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList;
+
+public class Ok {
+    public static void main(String[] args) {
+        // Demonstrating a heap-dynamic array using Java's ArrayList
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // Dynamically add elements
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(1, 15); // Insert 15 at index 1
+
+        System.out.println("\nHeap-Dynamic Array (ArrayList):");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        // Remove element at index 2
+        arr.remove(2);
+
+        System.out.println("\nAfter removal:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
 
